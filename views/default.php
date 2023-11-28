d6a0722011b639627c05ebd14ac8cda6b170a0c0
Christmann-Julian <julianchristmann@laposte.net>
2023-11-28 11:32:18 +0100

    ajout du logo à l'onglet du navigateur

--- views/default.php
+++ views/default.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Appli Frais</title>
+        <link rel="icon" type="image/png" href="assets/img/logo.png" >
         <?php 
         if(isset($css)){
             foreach ($css as $c) {
