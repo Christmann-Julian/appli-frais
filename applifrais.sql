5e5b4a03a3a091c67652509813b681ce173bcf77
Julian CHRISTMANN <julianchristmann.pro@gmail.com>
2024-03-04 15:52:37 +0100

    changement BDD

--- applifrais.sql
+++ applifrais.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 12 déc. 2023 à 10:56
--- Version du serveur : 8.2.0
--- Version de PHP : 8.1.24
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 04 mars 2024 à 15:50
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,12 +27,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `etat`
 --
 
-DROP TABLE IF EXISTS `etat`;
-CREATE TABLE IF NOT EXISTS `etat` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `libelle` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `etat` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `etat`
@@ -50,22 +48,21 @@ INSERT INTO `etat` (`id`, `libelle`) VALUES
 -- Structure de la table `fichedefrais`
 --
 
-DROP TABLE IF EXISTS `fichedefrais`;
-CREATE TABLE IF NOT EXISTS `fichedefrais` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fichedefrais` (
+  `id` int(11) NOT NULL,
   `mois` varchar(255) NOT NULL,
   `total` float NOT NULL,
   `date` date NOT NULL,
-  `idUtilisateur` int NOT NULL,
-  `idEtat` int NOT NULL,
-  `idFraisforfait` int NOT NULL,
-  `idFraishorsforfait` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idEtat` (`idEtat`),
-  KEY `idUtilisateur` (`idUtilisateur`),
-  KEY `idFraisforfait` (`idFraisforfait`),
-  KEY `idFraishorsforfait` (`idFraishorsforfait`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `idUtilisateur` int(11) NOT NULL,
+  `idEtat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `fichedefrais`
+--
+
+INSERT INTO `fichedefrais` (`id`, `mois`, `total`, `date`, `idUtilisateur`, `idEtat`) VALUES
+(1, '3', 102, '2024-03-04', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -73,15 +70,23 @@ CREATE TABLE IF NOT EXISTS `fichedefrais` (
 -- Structure de la table `fraisforfait`
 --
 
-DROP TABLE IF EXISTS `fraisforfait`;
-CREATE TABLE IF NOT EXISTS `fraisforfait` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fraisforfait` (
+  `id` int(11) NOT NULL,
   `libelle` varchar(255) NOT NULL,
-  `quantite` int NOT NULL,
+  `quantite` int(11) NOT NULL,
   `montant` float NOT NULL,
   `total` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `idFicheDeFrais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `fraisforfait`
+--
+
+INSERT INTO `fraisforfait` (`id`, `libelle`, `quantite`, `montant`, `total`, `idFicheDeFrais`) VALUES
+(1, 'Nuitée', 4, 3, 12, 1),
+(2, 'Repas Midi', 5, 2, 10, 1),
+(3, 'Kilométrage', 2, 3, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -89,16 +94,21 @@ CREATE TABLE IF NOT EXISTS `fraisforfait` (
 -- Structure de la table `fraishorsforfait`
 --
 
-DROP TABLE IF EXISTS `fraishorsforfait`;
-CREATE TABLE IF NOT EXISTS `fraishorsforfait` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fraishorsforfait` (
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `libelle` varchar(255) NOT NULL,
   `montant` float NOT NULL,
-  `idFicheDeFrais` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idFicheDeFrais` (`idFicheDeFrais`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `idFicheDeFrais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `fraishorsforfait`
+--
+
+INSERT INTO `fraishorsforfait` (`id`, `date`, `libelle`, `montant`, `idFicheDeFrais`) VALUES
+(1, '2024-03-21', 'fourniture de bureau', 17, 1),
+(2, '2024-03-07', 'matérielle médical', 57, 1);
 
 -- --------------------------------------------------------
 
@@ -106,18 +116,16 @@ CREATE TABLE IF NOT EXISTS `fraishorsforfait` (
 -- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `date_embauche` date NOT NULL,
   `role` varchar(255) NOT NULL,
-  `ville` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ville` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `utilisateurs`
@@ -128,6 +136,78 @@ INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `login`, `mdp`, `date_embauch
 (2, 'Dupont', 'Jean', 'jDupont', '$2y$10$6DR0njxQN9FtK7oB4UjQBuhj6B9g5xoOFDU36wO0HUO1HSdUl31o6', '2023-12-10', 'compta', 'Paris');
 
 --
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `etat`
+--
+ALTER TABLE `etat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `fichedefrais`
+--
+ALTER TABLE `fichedefrais`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUtilisateur` (`idUtilisateur`),
+  ADD KEY `idEtat` (`idEtat`);
+
+--
+-- Index pour la table `fraisforfait`
+--
+ALTER TABLE `fraisforfait`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idFicheDeFrais` (`idFicheDeFrais`);
+
+--
+-- Index pour la table `fraishorsforfait`
+--
+ALTER TABLE `fraishorsforfait`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idFicheDeFrais` (`idFicheDeFrais`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `etat`
+--
+ALTER TABLE `etat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `fichedefrais`
+--
+ALTER TABLE `fichedefrais`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `fraisforfait`
+--
+ALTER TABLE `fraisforfait`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `fraishorsforfait`
+--
+ALTER TABLE `fraishorsforfait`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Contraintes pour les tables déchargées
 --
 
@@ -135,15 +215,20 @@ INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `login`, `mdp`, `date_embauch
 -- Contraintes pour la table `fichedefrais`
 --
 ALTER TABLE `fichedefrais`
-  ADD CONSTRAINT `fichedefrais_ibfk_1` FOREIGN KEY (`idEtat`) REFERENCES `etat` (`id`),
-  ADD CONSTRAINT `fichedefrais_ibfk_2` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`id`),
-  ADD CONSTRAINT `fichedefrais_ibfk_3` FOREIGN KEY (`idFraisforfait`) REFERENCES `fraisforfait` (`id`);
+  ADD CONSTRAINT `fichedefrais_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `fichedefrais_ibfk_2` FOREIGN KEY (`idEtat`) REFERENCES `etat` (`id`);
+
+--
+-- Contraintes pour la table `fraisforfait`
+--
+ALTER TABLE `fraisforfait`
+  ADD CONSTRAINT `fraisforfait_ibfk_1` FOREIGN KEY (`idFicheDeFrais`) REFERENCES `fichedefrais` (`id`);
 
 --
 -- Contraintes pour la table `fraishorsforfait`
 --
 ALTER TABLE `fraishorsforfait`
-  ADD CONSTRAINT `fraishorsforfait_ibfk_1` FOREIGN KEY (`idFicheDeFrais`) REFERENCES `fichedefrais` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fraishorsforfait_ibfk_1` FOREIGN KEY (`idFicheDeFrais`) REFERENCES `fichedefrais` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
